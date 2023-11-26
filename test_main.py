301d01c358c594eba91ab2c21c23bbd87dd59710
Scrooge2727 <yaroslav_kadyrov@mail.ru>
2023-11-26 16:21:42 +0300

    Готовый

--- test_main.py
+++ test_main.py
@@ -1,5 +1,4 @@
 import unittest
-
 from main import Task, TaskList
 
 class TaskTest(unittest.TestCase):
@@ -13,6 +12,12 @@ class TaskTest(unittest.TestCase):
         task = Task("Buy groceries", "Milk, eggs, bread")
         task_info = task.get_task_info()
         expected_info = "Title: Buy groceries\nDescription: Milk, eggs, bread\nCompleted: False"
+        self.assertEqual(task_info, expected_info)
+
+    def test_task_info2(self):
+        task = Task("123", "123")
+        task_info = task.get_task_info()
+        expected_info = "Title: 123\nDescription: 123\nCompleted: False"
         self.assertEqual(task_info, expected_info)
 
     def test_task_default_completion(self):
@@ -39,8 +44,8 @@ class TaskListIntegrationTest(unittest.TestCase):
         self.task_list.add_task(task1)
         self.task_list.add_task(task2)
 
-        self.task_list.remove_task(task1)
-        self.assertListEqual(self.task_list.tasks, [task2])
+        self.task_list.remove_task(task2)  # Remove an existing task
+        self.assertListEqual(self.task_list.tasks, [task1])
 
     def test_get_all_tasks_info(self):
         task1 = Task("Buy groceries", "Milk, eggs, bread")
@@ -64,3 +69,14 @@ class TaskListIntegrationTest(unittest.TestCase):
 
         completed_tasks = self.task_list._get_completed_tasks()
         self.assertListEqual(completed_tasks, [task1])
+
+    def test_remove_nonexistent_task(self):
+        # Test removing a task that doesn't exist in the list
+        task1 = Task("Buy groceries", "Milk, eggs, bread")
+        task2 = Task("Complete project", "Finish the coding project")
+
+        self.task_list.add_task(task1)
+        with self.assertRaises(ValueError):
+            self.task_list.remove_task(task2)  # Attempt to remove a non-existent task
+
+
